9b6a53372dd3ff734b6577b5ca308ea0a01316d3
luan.sabino <luanphy.y@gmail.com>
2021-10-22 17:54:17 -0300

    Proxy para o netlify

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -3,11 +3,11 @@ export default class FetchApi {
   static async getBestsPodcasts() {
     var endpoint = '';
     if(window.location.hostname !== 'localhost')
-      endpoint="/api";
+      endpoint="/api/";
 
 
     let response = await fetch(
-      endpoint + "/best_podcasts?genre_id=93&page=1&region=us&safe_mode=0",
+      endpoint + "best_podcasts?genre_id=93&page=1&region=us&safe_mode=0",
       { method: "GET", headers: { "content-type": "application/json" } }
     ).then((res) => res.json());
     localStorage.setItem("best_podcasts", JSON.stringify(response));
@@ -16,10 +16,10 @@ export default class FetchApi {
   static async populateAnotherCategory(id, key) {
     var endpoint = '';
     if(window.location.hostname !== 'localhost')
-      endpoint="/api";
+      endpoint="/api/";
 
     let response = await fetch(
-      endpoint + `/best_podcasts?genre_id=93&page=1&region=us&safe_mode=0`,
+      endpoint + `best_podcasts?genre_id=93&page=1&region=us&safe_mode=0`,
       { method: "GET", headers: { "content-type": "application/json" } }
     ).then((res) => res.json());
     localStorage.setItem(key, JSON.stringify(response));
