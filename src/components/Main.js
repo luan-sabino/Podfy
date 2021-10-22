735bca536f4a67a482749d6e7189ef5cd32388c5
luan.sabino <luanphy.y@gmail.com>
2021-10-21 23:14:43 -0300

    Context de loading, e handler de windows size

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import Home from "./Home";
 
 export default function Main(){
+    
     return(
         <div className="w-full bg-main h-full overflow-auto">
             <Home></Home>
---
+++ src/services/isLoadingContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+const  isLoadingContext = createContext();
+export default isLoadingContext;
