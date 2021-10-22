bc4f1980395857ae162f89a0870a09098d6dede0
luan.sabino <luanphy.y@gmail.com>
2021-10-22 17:20:55 -0300

    Muda baseURL em produção

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,16 +1,25 @@
 export default class FetchApi {
 
   static async getBestsPodcasts() {
+    var endpoint = '';
+    if(window.location.hostname !== 'localhost')
+      endpoint="https://listen-api-test.listennotes.com/api/v2";
+
+
     let response = await fetch(
-      "/best_podcasts?genre_id=93&page=1&region=us&safe_mode=0",
+      endpoint + "/best_podcasts?genre_id=93&page=1&region=us&safe_mode=0",
       { method: "GET", headers: { "content-type": "application/json" } }
     ).then((res) => res.json());
     localStorage.setItem("best_podcasts", JSON.stringify(response));
   }
 
   static async populateAnotherCategory(id, key) {
+    var endpoint = '';
+    if(window.location.hostname !== 'localhost')
+      endpoint="https://listen-api-test.listennotes.com/api/v2";
+
     let response = await fetch(
-      `/best_podcasts?genre_id=93&page=1&region=us&safe_mode=0`,
+      endpoint + `/best_podcasts?genre_id=93&page=1&region=us&safe_mode=0`,
       { method: "GET", headers: { "content-type": "application/json" } }
     ).then((res) => res.json());
     localStorage.setItem(key, JSON.stringify(response));
